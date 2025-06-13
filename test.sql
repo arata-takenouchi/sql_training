67ce804763c98cd47c213f32c20cc206f0064fc3
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-06-13 22:05:22 +0900

    Q2

--- test.sql
+++ test.sql
@@ -6,3 +6,12 @@ SELECT group_name, MIN(ranking) as low_country, MAX(ranking) as high_country GRO
 SELECT group_name AS グループ, MIN(ranking) AS ランキング最上位, MAX(ranking) AS ランキング最下位
 FROM countries
 GROUP BY group_name
+
+-- 2. Find the average height and weight of goalkeepers
+-- my answer
+SELECT AVG(height), AVG(weight) FROM players WHERE position = 'GK';
+
+-- answer
+SELECT AVG(height) AS 平均身長, AVG(weight) AS 平均体重
+FROM players
+WHERE position = 'GK';
