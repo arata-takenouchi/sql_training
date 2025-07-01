732ce9d8067548c9d705b248804976845f7e62c2
arata-takenouchi <arata.takenouchi.1230@gmail.com>
2025-07-01 22:20:15 +0900

    Q17

--- test.sql
+++ test.sql
@@ -279,12 +279,12 @@ WHERE c1.group_name = 'C' AND c2.group_name = 'C'
 ORDER BY p1.kickoff, c1.ranking
 
 --answer
-SELECT p1.kickoff, c1.name AS my_country, c2.name AS enemy_country, 
+SELECT p1.kickoff, c1.name AS my_country, c2.name AS enemy_country,
     c1.ranking AS my_ranking, c2.ranking AS enemy_ranking,
     (SELECT COUNT(g1.id) FROM goals g1 WHERE p1.id = g1.pairing_id) AS my_goals,
     (
-        SELECT COUNT(g2.id) 
-        FROM goals g2 
+        SELECT COUNT(g2.id)
+        FROM goals g2
         LEFT JOIN pairings p2 ON p2.id = g2.pairing_id
         WHERE p2.my_country_id = p1.enemy_country_id AND p2.enemy_country_id = p1.my_country_id
     ) AS enemy_goals
@@ -293,3 +293,47 @@ LEFT JOIN countries c1 ON c1.id = p1.my_country_id
 LEFT JOIN countries c2 ON c2.id = p1.enemy_country_id
 WHERE c1.group_name = 'C' AND c2.group_name = 'C'
 ORDER BY p1.kickoff, c1.ranking
+
+
+-- 17. Show the number of goals scored in each match of Group C.
+-- my answer
+SELECT p1.kickoff, c1.name AS my_country, c2.name AS enemy_country,
+    c1.ranking AS my_ranking, c2.ranking AS enemy_ranking,
+    (SELECT COUNT(g1.id) FROM goals g1 WHERE p1.id = g1.pairing_id) AS my_goals,
+    (
+        SELECT COUNT(g2.id)
+        FROM goals g2
+        LEFT JOIN pairings p2 ON p2.id = g2.pairing_id
+        WHERE p2.my_country_id = p1.enemy_country_id AND p2.enemy_country_id = p1.my_country_id
+    ) AS enemy_goals,
+    my_goals - enemy_goals AS goal_diff
+FROM pairings p1
+LEFT JOIN countries c1 ON c1.id = p1.my_country_id
+LEFT JOIN countries c2 ON c2.id = p1.enemy_country_id
+WHERE c1.group_name = 'C' AND c2.group_name = 'C'
+ORDER BY p1.kickoff, c1.ranking
+
+--answer
+SELECT p1.kickoff, c1.name AS my_country, c2.name AS enemy_country,
+    c1.ranking AS my_ranking, c2.ranking AS enemy_ranking,
+    (SELECT COUNT(g1.id) FROM goals g1 WHERE p1.id = g1.pairing_id) AS my_goals,
+    (
+        SELECT COUNT(g2.id)
+        FROM goals g2
+        LEFT JOIN pairings p2 ON p2.id = g2.pairing_id
+        WHERE p2.my_country_id = p1.enemy_country_id AND p2.enemy_country_id = p1.my_country_id
+    ) AS enemy_goals,
+    (SELECT COUNT(g1.id) FROM goals g1 WHERE p1.id = g1.pairing_id) - 
+    (
+        SELECT COUNT(g2.id)
+        FROM goals g2
+        LEFT JOIN pairings p2 ON p2.id = g2.pairing_id
+        WHERE p2.my_country_id = p1.enemy_country_id AND p2.enemy_country_id = p1.my_country_id
+    ) AS goal_diff
+FROM pairings p1
+LEFT JOIN countries c1 ON c1.id = p1.my_country_id
+LEFT JOIN countries c2 ON c2.id = p1.enemy_country_id
+WHERE c1.group_name = 'C' AND c2.group_name = 'C'
+ORDER BY p1.kickoff, c1.ranking
+-- サブクエリの結果のカラムを使った計算はできない（構文エラー）
+-- なので、サブクエリを2回書くことになる
